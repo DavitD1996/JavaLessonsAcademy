264a7ea5071b55dbb51975bcb7431c74f6bfdb99
DavitD1996 <davit.sargsyan@picsart.com>
2023-02-19 20:28:51 +0400

    the functional of sheet is final

--- src/main/java/myProject/Main.java
+++ src/main/java/myProject/Main.java
@@ -1,36 +1,51 @@
 package myProject;
 
 import com.sun.source.tree.Tree;
+import myProject.excel.CellBase;
 
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
-           TreeMap<Integer,String>a=new TreeMap<>();
-           a.put(1,"aa");
-           a.put(2,"bb");
-           a.put(3,"cc");
-           a.put(6,"vv");
-           AtomicInteger max= new AtomicInteger();
-           System.out.println( a.entrySet().size());
-          /*for(int i=1;i<a.size();i++){
-           System.out.println(a.get(i));
-       }*/
+
     }
     public static void setRowBellow(int row,HashMap<Integer,String> test){
-        if(test.get(row+1)==null){
-            test.put(row+1,"new");
+        Integer lastIndex=(Integer) test.keySet().toArray()[test.size()-1];
+        while(lastIndex>row){
+            test.put(++lastIndex,test.get(--lastIndex));
+            test.put(lastIndex,null);
+            --lastIndex;
         }
-        else{
-            int index=row+1;
-            while(test.get(index)!=null){
-                index++;
-            }
-            while(index>row){
-                test.put(index,test.get(--index));
-                test.put(index--,null);
-            }
+}
+public  static void removeRow(int row,HashMap<Integer,String> test){
+    Integer lastIndex=(Integer) test.keySet().toArray()[test.size()-1];
+    while(row<=lastIndex){
+        test.put(row,test.get(++row));
+    }
+}
+public static void addColumn(int column, HashMap<Integer,HashMap<Integer,String>>data){
+    Object[]keys=data.keySet().toArray();
+    for(int i=0;i<keys.length;i++){
+        HashMap<Integer, String>temp=data.get((Integer)keys[i]);
+        Integer lastIndex=(Integer) temp.keySet().toArray()[temp.size()-1];
+        while(lastIndex>=column){
+            temp.put(++lastIndex,temp.get(--lastIndex));
+            temp.put(lastIndex,null);
+            --lastIndex;
         }
-}}
+    }
+}
+public static void swapRows(int row1, int row2,HashMap<Integer,HashMap<Integer,String>>data){
+         if(data.get(row1)==null&&data.get(row2)==null){
+             System.out.println("no sense to swap the empty rows");
+             return;
+         }
+         HashMap<Integer,String>temp=data.get(row1);
+         data.put(row1,data.get(row2));
+         data.put(row2,temp);
+}
+}
--- src/main/java/myProject/excel/Spreadsheet.java
+++ src/main/java/myProject/excel/Spreadsheet.java
@@ -26,17 +26,60 @@ public class Spreadsheet {
         }
     }
     public void setRowBellow(int row){
-        if(excelsheet.get(row+1)==null){
-            excelsheet.put(row+1, new HashMap<>());
-        }
-        else{
-            int index=row+1;
-            while(excelsheet.get(index)!=null){
-                index++;
+            Integer lastIndex=(Integer) excelsheet.keySet().toArray()[excelsheet.size()-1];
+            while(lastIndex>row){
+                excelsheet.put(++lastIndex,excelsheet.get(--lastIndex));
+                excelsheet.put(lastIndex,null);
+                lastIndex--;
             }
-            while(index>row){
-               excelsheet.put(index,excelsheet.get(index--));
-               excelsheet.put(index--,null);
+    }
+    public void removeRow(int row){
+        Integer lastIndex=(Integer) excelsheet.keySet().toArray()[excelsheet.size()-1];
+        while(row<=lastIndex){
+            excelsheet.put(row,excelsheet.get(++row));
+        }
+    }
+    public void addColumnFromRight(int column){
+        Object[]keys=excelsheet.keySet().toArray();
+        for(int i=0;i<keys.length;i++){
+            HashMap<Integer, CellBase>temp=excelsheet.get((Integer)keys[i]);
+            Integer lastIndex=(Integer) temp.keySet().toArray()[temp.size()-1];
+            while(lastIndex>=column){
+                temp.put(++lastIndex,temp.get(--lastIndex));
+                temp.put(lastIndex,null);
+                --lastIndex;
+            }
+        }
+    }
+    public  void swapRows(int row1, int row2){
+            if(excelsheet.get(row1)==null&&excelsheet.get(row2)==null){
+                System.out.println("no sense to swap the empty rows");
+                return;
+            }
+           else if(excelsheet.get(row1)==null){
+                excelsheet.put(row1,new HashMap<>());
+            }
+           else if(excelsheet.get(row2)==null){
+                excelsheet.put(row2,new HashMap<>());
+            }
+            HashMap<Integer,CellBase>temp=excelsheet.get(row1);
+            excelsheet.put(row1,excelsheet.get(row2));
+            excelsheet.put(row2,temp);
+    }
+    public void swapColumns(int col1, int col2){
+        Object[]keys=excelsheet.keySet().toArray();
+        for(int i=0;i<keys.length;i++){
+            CellBase temporary=excelsheet.get((Integer)keys[i]).get(col1);
+            excelsheet.get((Integer)keys[i]).put(col1,excelsheet.get((Integer)keys[i]).get(col2));
+            excelsheet.get((Integer)keys[i]).put(col2,temporary);
+        }
+    }
+    public void removeColumn(int column){
+        Object[]keys=excelsheet.keySet().toArray();
+        Integer lastIndex=(Integer) excelsheet.keySet().toArray()[excelsheet.size()-1];
+        for(int i=0;i<keys.length;i++){
+            while(column<=lastIndex){
+                excelsheet.put(column,excelsheet.get(++column));
             }
         }
     }
